9038f472dbaa4e7dbfdcdfdca8a7c1206f0a2c34
Joseph Heckman <97683394+joeheckman@users.noreply.github.com>
2025-01-17 12:29:50 -0500

    Update Syk_twopoint_sparsification.py

--- BeginSYKsimulations/Syk_twopoint_sparsification.py
+++ BeginSYKsimulations/Syk_twopoint_sparsification.py
@@ -1,7 +1,4 @@
-#current time: 1.234 seconds for N=10. Sparsification is slower at low N (my computer) and faster at larger N. That's decent news. 
-#Last test showed that generating the Hamiltonian for larger N (22 on 1.13.25) is very doable. 
-#However, two-point calculation for this Hamiltonian is still much too slow. 
-#By the way, Mathematica is 1000 times faster and simpler for sparse matrix multiplication. 
+##N=20 simulation sparse matrices first implementation
 import numpy as np
 import scipy.linalg as linalg
 from scipy.sparse import csr_matrix,vstack,kron, save_npz
@@ -9,7 +6,7 @@ from scipy.sparse.linalg import expm as sparse_expm
 import matplotlib.pyplot as plt
 import time
 start_time = time.time()
-N_maj = 22
+N_maj = 20
 
 ##find dtype=complex is the homework problem
 op_size = 2**(N_maj/2)
@@ -29,10 +26,6 @@ left_petal = [pauliz for _ in range(int(N_maj/2)-1)]
 right_petal = [identity for _ in range(int(N_maj/2)-1)]
 even_pistil = [paulix for _ in range(1)] 
 odd_pistil = [pauliy for _ in range(1)]
-
-left_petal_sparse = [csr_matrix(pauliz) for _ in range(int(N_maj/2)-1)]
-right_petal_sparse  = [csr_matrix(identity) for _ in range(int(N_maj/2)-1)]
-
 
 chi_even_builder = [*left_petal, *even_pistil, *right_petal] ## a flower
 chi_odd_builder = [*left_petal, *odd_pistil, *right_petal]   ## another one, different species, yellower
@@ -62,9 +55,8 @@ for i in range(int(N_maj/2)-1,-1,-1):
         
 Hamiltonian = np.zeros((int(op_size),int(op_size)),dtype=complex)
 sparse_Hamiltonian = csr_matrix((int(op_size),int(op_size)), dtype=complex)
-
 mean = 0
-std_dev = np.sqrt(((6))/(16*N_maj**3))
+std_dev = np.sqrt(((6))/(N_maj**3))
 
 ##make arrays of fermions 
 Fermions = np.vstack((chiEven_loop,chiOdd_loop))
@@ -83,37 +75,46 @@ for i in range(0,int(N_maj)):
                 print(f'Chi{i}*Chi{j}*Chi{k}*Chi{l}')
                 randomNumber = np.random.normal(mean,std_dev)
                 
+ #               Hamiltonian[:,:] += randomNumber*(Fermions[i,:,:] @ Fermions[j,:,:] @ Fermions[k,:,:] @ Fermions[l,:,:])
                 sparse_Hamiltonian += randomNumber*(sparse_Fermions[i] @ sparse_Fermions[j] @ sparse_Fermions[k] @ sparse_Fermions[l])
 endtime = time.time()
+totaltime = endtime-start_time
+print(f' Hamiltonian: {totaltime}')
 
 ##this is for 4 interaction term, (i^(interaction terms/2))
-sparse_Hamiltonian = -1*sparse_Hamiltonian
+sparse_Hamiltonian = (-1/24)*sparse_Hamiltonian
+#Hamiltonian = (-1/(24))*Hamiltonian
 # diving into the 2point here 
 beta = 1
-range_values = np.arange(0,beta,.01) ##Periodicity at beta right?
+range_values = np.arange(0,beta/2,beta/40) ##Periodicity at beta right?
 array_length = len(range_values)
+new_array = np.zeros((int(N_maj),array_length))
 new_array2 = csr_matrix((int(N_maj),array_length))
-new_array2Test = csr_matrix((int(N_maj),array_length))
 
-imaginary_time_evo = sparse_expm(-beta*sparse_Hamiltonian) 
-#for i in range(0,int(N_maj)): 
-i = 1;
-count = -1
-for tau in np.arange(0,beta,.01):
-    print(tau)
-    count += 1
-    
-    ##1.13.25 gets totally stuck here. 
-    
-    
-    #time_evo_operator is its own thing to keep all of the time evolution parts attached to a Majorana operator
-    time_evo_operator = csr_matrix(sparse_expm((-beta+tau)*sparse_Hamiltonian) @ Fermions[i] @ sparse_expm(-1*sparse_Hamiltonian*tau))  ##they win again, that being the youtube video Leo and Evan keep telling me to watch and GPT
-    #point = np.trace(time_evo_operator @ Fermions[i,:,:]) / np.trace(linalg.expm((-beta+tau)*Hamiltonian) @ linalg.expm(-1*Hamiltonian*tau))
-    point = (time_evo_operator @ sparse_Fermions[i]) / (sparse_expm((-beta+tau)*sparse_Hamiltonian) @ sparse_expm(-1*sparse_Hamiltonian))    
-    point_trace = point.trace()
-    
-    new_array2[i,count]=point_trace
+test = np.zeros((int(N_maj),array_length))
+test2 = csr_matrix((int(N_maj),array_length))
 
-twoPoint = np.mean(new_array2,axis=0)
-totaltime = endtime-start_time
-print(totaltime)
+for i in range(0,int(N_maj)): 
+    count = -1
+    
+    for tau in np.arange(0,beta/2,beta/40):
+        print(tau)
+        start_time_2 = time.time()
+        count += 1
+        
+        
+        #time_evo_operator is its own thing to keep all of the time evolution parts attached to a Majorana operator
+        time_evo_operator = sparse_expm((-beta+tau)*sparse_Hamiltonian) @ sparse_Fermions[i] @ sparse_expm(-1*sparse_Hamiltonian*tau)
+        #time_evo_operator_normal = linalg.expm((-beta+tau)*Hamiltonian) @ Fermions[i,:,:] @ linalg.expm(-1*Hamiltonian*tau)
+        
+        
+        #point = np.trace(time_evo_operator_normal @ Fermions[i,:,:]) / np.trace(linalg.expm((-beta+tau)*Hamiltonian) @ linalg.expm(-1*Hamiltonian*tau))
+        point_sparse = (time_evo_operator @ sparse_Fermions[i]).trace() / ((sparse_expm((-beta+tau)*sparse_Hamiltonian) @ sparse_expm(-1*sparse_Hamiltonian*tau))).trace() 
+        
+       #new_array[i,count]=point
+        new_array2[i,count] = point_sparse
+        end_time_2 = time.time()
+        total2 = end_time_2 - start_time_2
+        print(total2)
+
+twoPoint = np.mean(new_array,axis=0)
